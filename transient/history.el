fdeab8c344e8433077bebaff6f14dcabb0003ca2
nebhrajani-a <aditya.v.nebhrajani@gmail.com>
2020-07-28 12:40:13 +0530

    Untracked files issue resolved to fix .gitignore again

--- transient/history.el
+++
@@ -1,9 +0,0 @@
-((magit-commit nil)
- (magit-diff
-  (("--" "README.md")
-   "--no-ext-diff" "--stat"))
- (magit-dispatch nil)
- (magit-merge nil)
- (magit-push nil)
- (magit:-- "README.md" "")
- (magithub-dispatch-popup nil))
