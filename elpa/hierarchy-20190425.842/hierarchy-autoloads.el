4769576301237765e77e6b7ff9c683837e7b602d
nebhrajani-a <aditya.v.nebhrajani@gmail.com>
2020-11-13 11:37:38 +0530

    Added evil-matchit for jumping surrounds with %.

---
+++ elpa/hierarchy-20190425.842/hierarchy-autoloads.el
@@ -0,0 +1,15 @@
+;;; hierarchy-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
+
+;;;### (autoloads nil nil ("hierarchy.el") (24457 15837 228692 805000))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; hierarchy-autoloads.el ends here
